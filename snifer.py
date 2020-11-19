1f5d298f070ad1182fc1c4f8b9fec9984139d59c
ST2123 <sandrotyavadze@gmail.com>
2020-11-19 07:47:02 -0500

    decode(ICMP,TCP,UDP)

---
+++ snifer.py
@@ -0,0 +1,11 @@
+import decode
+import socket
+import pprint
+
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+
+data_on_port={}
+
+while True:   
+    data = s.recvfrom(65565)
+    pprint.pprint(decode.Decode_packet(data[0]))
